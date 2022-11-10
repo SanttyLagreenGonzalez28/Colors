e4f5c148f06513dc1736789de511468c8f8a5e5d
SanttyLagreenGonzalez28 <115585238+SanttyLagreenGonzalez28@users.noreply.github.com>
2022-11-10 16:08:46 -0500

    Add files via upload
    Tarea colores 
    Hola profe perdón había tenido problemas con mi Git hub

---
+++ ejerciciocolores.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class ejerciciocolores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int azul, amarillo, verde;
+		
+		int opcion = Integer.parseInt(JOptionPane.showInputDialog("Escoja que color quiere usar: "
+				+ " + \"1. azul\"\r\n"
+				+ "+ \"2. amarillo\""));
+		     
+		azul = Integer.parseInt(JOptionPane.showInputDialog("Esccoja el numero 1"));
+		amarillo = Integer.parseInt(JOptionPane.showInputDialog("Escoja el numero 2"));
+		
+		if (opcion= 1);{
+			
+		}
+		  		
+		
+
+	}
+
+}
+/*Mezclador de colores (ingresar dos colores primarios y retornar la mezcla entre ellos)
+ * 
+ */
+
+
